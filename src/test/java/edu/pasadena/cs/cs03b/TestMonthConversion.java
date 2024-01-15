a74d1067f4d957039d7cc3d67e57a7a441e30354
Ken Chang - CS Professor <154487739+adjprof2@users.noreply.github.com>
2024-01-15 12:55:46 -0800

    Update TestMonthConversion.java

--- src/test/java/edu/pasadena/cs/cs03b/TestMonthConversion.java
+++ src/test/java/edu/pasadena/cs/cs03b/TestMonthConversion.java
@@ -20,13 +20,56 @@ public class TestMonthConversion {
 
      // action
      MonthConversion.main(new String[]{"1"});
-
      // assertion
      assertEquals("January\n", bos.toString());
 
      MonthConversion.main(new String[]{"2"});
      // assertion
      assertEquals("February\n", bos.toString());
+
+     MonthConversion.main(new String[]{"3"});
+     // assertion
+     assertEquals("March\n", bos.toString());
+
+     MonthConversion.main(new String[]{"4"});
+     // assertion
+     assertEquals("April\n", bos.toString());
+
+     MonthConversion.main(new String[]{"5"});
+     // assertion
+     assertEquals("May\n", bos.toString());
+
+     MonthConversion.main(new String[]{"6"});
+     // assertion
+     assertEquals("June\n", bos.toString());
+      
+     MonthConversion.main(new String[]{"7"});
+     // assertion
+     assertEquals("July\n", bos.toString());
+
+     MonthConversion.main(new String[]{"8"});
+     // assertion
+     assertEquals("August\n", bos.toString());
+      
+     MonthConversion.main(new String[]{"9"});
+     // assertion
+     assertEquals("September\n", bos.toString());
+
+     MonthConversion.main(new String[]{"10"});
+     // assertion
+     assertEquals("October\n", bos.toString());
+
+     MonthConversion.main(new String[]{"11"});
+     // assertion
+     assertEquals("November\n", bos.toString());
+
+     MonthConversion.main(new String[]{"12"});
+     // assertion
+     assertEquals("December\n", bos.toString());
+
+     MonthConversion.main(new String[]{"13"});
+     // assertion
+     assertEquals("No Such Month\n", bos.toString());
 
      // undo the binding in System
      System.setOut(originalOut);
