b5ad7ec719e6b809088619df1eb1ae9cc754431b
Alice Dong <100387325+all-ice3@users.noreply.github.com>
2024-01-22 18:05:14 +0000

    Added a break for line 21 Filled in the switch case for the rest of the months between lines 25-42 Added a default case on line 44

--- src/main/java/edu/pasadena/cs/cs03b/MonthConversion.java
+++ src/main/java/edu/pasadena/cs/cs03b/MonthConversion.java
@@ -17,12 +17,33 @@ public class MonthConversion {
                 monthString = "January";
                 break;
             case 2:  monthString = "February";
-            // TODO: what is missing here
+                // TODO: what is missing here
+                break;
             case 3:  monthString = "March";
-                     break;
+                break;
             // TODO: Add the rest of the months here
+            case 4:  monthString = "April";
+                break;
+            case 5:  monthString = "May";
+                break;
+            case 6:  monthString = "June";
+                break;
+            case 7:  monthString = "July";
+                break;
+            case 8:  monthString = "August";
+                break;
+            case 9:  monthString = "September";
+                break;
+            case 10:  monthString = "October";
+                break;
+            case 11:  monthString = "November";
+                break;
+            case 12:  monthString = "December";
+                break;
             // TODO: add a default case to output "No Such Month"
+            default: monthString = "No Such Month";
         }
+        
         
         return monthString;
     }
